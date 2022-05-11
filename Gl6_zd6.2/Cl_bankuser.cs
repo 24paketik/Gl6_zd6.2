747b731c7e6c3af99ca1fa3cbd5e298509c023a5
24paketik <deniska-smirnov-05@mail.ru>
2022-05-11 13:29:54 +0300

    create class

---
+++ Gl6_zd6.2/Cl_bankuser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gl6_zd6._2
+{
+    public class Cl_bankuser
+    {
+        public string Surname;
+        public int NumPass;
+        public byte[] AmCheck = new byte[5];
+        public int[] NumCheck = new int[5];
+        public byte[] PrYear = new byte[5];
+        public double[] Balance = new double[5];
+
+    }
+}
